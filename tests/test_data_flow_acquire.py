067d2370e567c4d073980c2197a8aab7465057c7
Beste Aydemir <besteaydemir99@gmail.com>
2021-03-16 15:48:52 +0300

    FE

--- tests/test_data_flow_acquire.py
+++ tests/test_data_flow_acquire.py
@@ -3,6 +3,8 @@ from flow.system_flow import SystemFlow
 from utils.animate_live import AnimateLive
 import numpy as np
 
+data_dim = 9
+
 props = {'xStream': {}, 'IForest': {}, 'Loda': {}}
 
 ens_props = {'Ensemble (IForest and Loda)': {'IForest': {}, 'Loda': {}},
@@ -10,7 +12,7 @@ ens_props = {'Ensemble (IForest and Loda)': {'IForest': {}, 'Loda': {}},
              'Ensemble (xStream and IForest)': {'xStream': {}, 'IForest': {}},}
 
 #param is dictionary of dictionaries
-param = {'autoencoder': {'latent_dim': 9,
+param = {'autoencoder': {'latent_dim': data_dim,
                          'batch_size': 1,
                          'epoch_no': 5,
                          'optimizer': 'adam',
@@ -31,7 +33,7 @@ selected_features = {'autoencoder': col_names
 selected_feature_extractors = ['autoencoder']
 
 fe_config = {"selected_feature_extractors": selected_feature_extractors, "selected_features": selected_features, "param": param}
-system_flow = SystemFlow(122, props, ens_props, config="cloud", fe=True, fe_config=fe_config, verbose=True)
+system_flow = SystemFlow(data_dim, props, ens_props, config="cloud", fe=True, fe_config=fe_config, verbose=True)
 topic = "beste"
 system_flow.create_stream(topic)
 anim = AnimateLive(ax_num=6, x_labels=["Time"] * 6, y_labels=["xStream", "IForest", "Loda", "Ensemble (IForest and Loda)",
