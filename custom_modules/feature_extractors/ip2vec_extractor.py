504a6dd327aad0439374ba3610ec4ae425b4037f
berfinkavsut <69820094+berfinkavsut@users.noreply.github.com>
2021-05-30 22:33:58 +0300

    test-commit

--- custom_modules/feature_extractors/ip2vec_extractor.py
+++ custom_modules/feature_extractors/ip2vec_extractor.py
@@ -66,7 +66,7 @@ class Ip2VecExtractor(BaseFeatureExtractor):
         feature_vectors = []
 
         for i in range(X.shape[0]):
-
+            print(i)
             pkt = X[i]
             feature_vector = np.array([])
             for j in range(X.shape[1]):
@@ -94,3 +94,22 @@ class Ip2VecExtractor(BaseFeatureExtractor):
         return self.features_extracted
 
 
+os.chdir('..')
+os.chdir('..')
+
+main_dir = os.getcwd()
+data_dir = os.path.join(main_dir, "data")
+file_dir = os.path.join(data_dir, "Kitsune_45000_not_transformer.csv")
+X = pd.read_csv(file_dir)
+
+selected_features = ['source_ip', 'destination_ip', 'dst_port', 'protocol_name']
+X.dropna(subset=selected_features, inplace=True)
+
+param = {'emb_dim': 10, 'max_epoch': 50, 'batch_size': 128, 'neg_num': 10}
+
+ip2vec_extractor = Ip2VecExtractor(param=param, selected_features=selected_features)
+
+ip2vec_extractor.fit(X)
+features_extracted = ip2vec_extractor.transform(X)
+print('done')
+features_extracted.to_csv("ip2vec_4_feature.csv")
