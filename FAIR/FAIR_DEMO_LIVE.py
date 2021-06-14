66b2cc66961fd11da0ee7e1486a64d92a0b60e2a
berfinkavsut <69820094+berfinkavsut@users.noreply.github.com>
2021-06-14 15:42:53 +0300

    test-commit

--- FAIR/FAIR_DEMO_LIVE.py
+++ FAIR/FAIR_DEMO_LIVE.py
@@ -15,32 +15,26 @@ param = {'autoencoder': {'latent_dim': data_dim,
                          'batch_size': 1,
                          'epoch_no': 5,
                          'optimizer': 'adam',
-                         'loss': 'mse'},
-         'lstm_autoencoder': {'latent_dim': 10,
-                              'time_step': 1,  # will be fixed
-                              'epoch_no': 5,
-                              'optimizer': 'adam',
-                              'loss': 'mse'},
-         }
+                         'loss': 'mse'}}
 
-col_names = ["duration", "source_ip", "destination_ip", "protocol", "packet_len", "dif_serv", "flag", "ip_vers", "src_port", "dst_port",
-             "data_len",
-             "seq", "seq_raw", "next_seq", "ack", "ack_raw", "flags_res", "flags_ns", "flags_cwr", "flags_ecn", "flags_urg", "flags_ack",
-             "flags_push",
-             "flags_reset", "flags_syn", "flags_fin", "win_size", "checksum", "checksum_status", "urgent_pointer", "proto_type", "proto_size",
-             "hw_type",
-             "hw_size", "hw_opcode", "src_hw_mac", "dst_hw_mac"]
+col_names = ["duration", "source_ip", "destination_ip", "protocol", "packet_len", "dif_serv", "flag", "ip_vers",
+             "src_port", "dst_port", "data_len", "seq", "seq_raw", "next_seq", "ack", "ack_raw", "flags_res",
+             "flags_ns", "flags_cwr", "flags_ecn", "flags_urg", "flags_ack", "flags_push", "flags_reset", "flags_syn",
+             "flags_fin", "win_size", "checksum", "checksum_status", "urgent_pointer", "proto_type", "proto_size",
+             "hw_type", "hw_size", "hw_opcode", "src_hw_mac", "dst_hw_mac"]
 
-selected_features = {'autoencoder': col_names,
-                     'lstm_autoencoder': col_names}
+selected_features = {'autoencoder': col_names}
+selected_feature_extractors = ['autoencoder']
 
-selected_feature_extractors = ['lstm_autoencoder']
-
-fe_config = {"selected_feature_extractors": selected_feature_extractors, "selected_features": selected_features, "param": param}
+fe_config = {"selected_feature_extractors": selected_feature_extractors,
+             "selected_features": selected_features,
+             "param": param
+             }
 
 topic = "Device1"
 system_flow = SystemFlow( props, ens_props, config="cloud", fe=True, fe_config=fe_config, user="elastic",
                  psw="changeme", elk_index="test_flow", verbose=True, with_elastic=True, with_dataset=False)
+
 
 system_flow.create_stream(topic)
 
@@ -69,7 +63,7 @@ while True:
             probsarray.append(ens_probs[key][0])
 
         max_prob = max(probsarray)
-        print("maxprob" , max_prob)
+        print("maxprob", max_prob)
 
         if threshold < max_prob:
             alarm.send_email()
@@ -87,4 +81,3 @@ while True:
         #     system_flow.update(hyperparameters)
         #
         # old_hyp = hyperparameters
-
